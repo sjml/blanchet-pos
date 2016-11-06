74035cd815adc06b25bec762a56e5d71325417e3
Shane Liesegang <shane@techie.net>
2016-11-05 19:21:29 -0700

    de-duping and playing with callbacks

--- script.js
+++ script.js
@@ -3,21 +3,39 @@ $(document).ready(function() {
     $('.tile').on('click', onTileClick);
 });
 
+$.fn.extend({
+    animateCss: function (animationName, callback) {
+        var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        this.addClass('animated ' + animationName).one(animationEnd, function() {
+            $(this).removeClass('animated ' + animationName);
+            if (callback) {
+                callback.bind(this)();
+            }
+        });
+    },
+});
+
 function onTileClick(event) {
     var clicked = event.currentTarget;
     var index = $(clicked).index();
     var total = $(clicked).siblings().length + 1;
+
+    var lockedWidth = $(clicked).width();
+
     $('.tile').each(function() {
         if (this != clicked) {
             if ( $(this).index() < index ) {
-                $(this).addClass('animated fadeOutLeft');
+                $(this).animateCss('fadeOutLeft');
             }
             else {
-                $(this).addClass('animated fadeOutRight');
+                $(this).animateCss('fadeOutRight');
             }
         }
         else {
             // we have other plans for you
+            // $(clicked).width(lockedWidth);
+
+
             var destination = clicked.dataset.page;
             // window.history.pushState({}, destination, destination);
         }
